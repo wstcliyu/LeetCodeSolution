43ab41456df34403f1536b1e654abd767ebd1b12
Yu Li <aylian@amazon.com>
2021-03-24 00:40:30 -0700

    438. Find All Anagrams in a String

--- Solution-Go/Find_438.go
+++ Solution-Go/Find_438.go
@@ -7,16 +7,15 @@ func findAnagrams(s string, p string) []int {
     for _, c := range p {
         freq[c] += 1
     }
-    l, r, count := 0, 0, len(p)
-    for r < len(s) {
-        if freq[rune(s[r])] -= 1; freq[rune(s[r])] >= 0 {
+    l, count := 0, len(p)
+    for r, c := range s {
+        if freq[c] -= 1; freq[c] >= 0 {
             count -= 1
         }
-        r += 1
         if count == 0 {
             res = append(res, l)
         }
-        if r - l == len(p) {
+        if r - l + 1 == len(p) {
             if freq[rune(s[l])] += 1; freq[rune(s[l])] > 0 {
                 count += 1
             }
