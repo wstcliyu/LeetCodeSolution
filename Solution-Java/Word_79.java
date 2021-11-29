e2d4eb0e0f845cc8744b38db2598f4f3083836dd
Yu Li <wstcliyu@gmail.com>
2021-11-28 21:50:58 -0800

    79. Word Search

--- Solution-Java/Word_79.java
+++ Solution-Java/Word_79.java
@@ -4,22 +4,23 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, 0, word, i, j)) return true;
+                if (board[i][j] == word.charAt(0) && dfs(board, 0, word, i, j)) return true;
             }
         }
         return false;
     }
     
-    private boolean dfs(char[][] board, int start, String word, int i, int j) {
-        if (start == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(start)) return false;
+    private boolean dfs(char[][] board, int idx, String word, int i, int j) {
+        if (idx == word.length() - 1) return true;
         board[i][j] = '#';
         for (int[] d : dirs) {
-            int x = i + d[0];
-            int y = j + d[1];
-            if (dfs(board, start + 1, word, x, y)) return true;
+            int x = i + d[0], y = j + d[1];
+            if (x >= 0 && x < board.length && y >= 0 && y < board[0].length 
+                && board[x][y] == word.charAt(idx + 1) && dfs(board, idx + 1, word, x, y)) {
+                return true;
+            }
         }
-        board[i][j] = word.charAt(start);
+        board[i][j] = word.charAt(idx);
         return false;
     }
 }
