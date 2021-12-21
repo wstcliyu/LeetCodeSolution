f3966b1b6f3ae28795e1477e16b29b31e126c60c
Yu Li <wstcliyu@gmail.com>
2021-12-20 21:24:49 -0800

    45. Jump Game II

--- Solution-Java/Jump_45.java
+++ Solution-Java/Jump_45.java
@@ -37,3 +37,22 @@ class Solution {
         return jumps;
     }
 }
+
+
+
+
+// My updated BFS
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int start = 0;
+        int queueEnd = 0;
+        while (start <= queueEnd && queueEnd < nums.length - 1) {
+            for (int i = queueEnd; i >= start; i--) {
+                queueEnd = Math.max(queueEnd, i + nums[i]);
+            }
+            jumps++;
+        }
+        return jumps;
+    }
+}
