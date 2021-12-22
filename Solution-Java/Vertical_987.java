f28e444b210be944765868439e3e15430fc59631
Yu Li <wstcliyu@gmail.com>
2021-12-22 00:12:29 -0800

    987. Vertical Order Traversal of a Binary Tree

---
+++ Solution-Java/Vertical_987.java
@@ -0,0 +1,319 @@
+// https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231125/Java-HashMap-and-TreeMap-and-PriorityQueue-Solution
+// Java HashMap & TreeMap & PriorityQueue
+class Solution {
+    Map<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new HashMap<>();
+    int minX = 0, maxX = 0;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        helper(root, 0, 0);
+        List<List<Integer>> vertical = new ArrayList<>();
+        for (int i = minX; i <= maxX; i++) {
+            List<Integer> level = new ArrayList<Integer>();
+            for (int key : map.get(i).keySet()) {
+                while (!(map.get(i).get(key)).isEmpty()) {
+                    level.add(map.get(i).get(key).poll());
+                }
+            }
+            vertical.add(level);
+        }
+        return vertical;
+        
+    }
+    
+    private void helper(TreeNode node, int x, int y) {
+        if (node == null) return;
+        minX = Math.min(minX, x);
+        maxX = Math.max(maxX, x);
+        if (map.get(x) == null) { map.put(x, new TreeMap<Integer, PriorityQueue<Integer>>()); }
+        if (map.get(x).get(y) == null) { map.get(x).put(y, new PriorityQueue<Integer>()); }
+        map.get(x).get(y).add(node.val);
+        helper(node.left, x - 1, y + 1);
+        helper(node.right, x + 1, y + 1);   
+
+    }
+}
+
+
+
+
+// Approach 2: BFS/DFS with Partition Sorting
+// BFS
+class Solution {
+    // key: column; value: <row, node_value>
+    Map<Integer, ArrayList<Pair<Integer, Integer>>> columnTable = new HashMap();
+    int minColumn = 0, maxColumn = 0;
+
+    private void BFS(TreeNode root) {
+        // tuples of <column, <row, value>>
+        Queue<Pair<TreeNode, Pair<Integer, Integer>>> queue = new ArrayDeque();
+        int row = 0, column = 0;
+        queue.offer(new Pair(root, new Pair(row, column)));
+
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Pair<Integer, Integer>> p = queue.poll();
+            root = p.getKey();
+            row = p.getValue().getKey();
+            column = p.getValue().getValue();
+
+            if (root != null) {
+                if (!columnTable.containsKey(column)) {
+                    columnTable.put(column, new ArrayList<Pair<Integer, Integer>>());
+                }
+                columnTable.get(column).add(new Pair<>(row, root.val));
+                minColumn = Math.min(minColumn, column);
+                maxColumn = Math.max(maxColumn, column);
+
+                queue.offer(new Pair(root.left, new Pair(row + 1, column - 1)));
+                queue.offer(new Pair(root.right, new Pair(row + 1, column + 1)));
+            }
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> output = new ArrayList();
+        if (root == null) {
+            return output;
+        }
+
+        // step 1). BFS traversal
+        this.BFS(root);
+
+        // step 2). retrieve the value from the columnTable
+        for (int i = minColumn; i < maxColumn + 1; ++i) {
+            // order by both "row" and "value"
+            Collections.sort(columnTable.get(i), new Comparator<Pair<Integer, Integer>>() {
+                @Override
+                public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
+                    if (p1.getKey().equals(p2.getKey()))
+                        return p1.getValue() - p2.getValue();
+                    else
+                        return p1.getKey() - p2.getKey();
+                }
+            });
+
+            List<Integer> sortedColumn = new ArrayList();
+            for (Pair<Integer, Integer> p : columnTable.get(i)) {
+                sortedColumn.add(p.getValue());
+            }
+            output.add(sortedColumn);
+        }
+
+        return output;
+    }
+}
+
+
+
+
+// Approach 2: BFS/DFS with Partition Sorting
+// DFS
+class Solution {
+    Map<Integer, ArrayList<Pair<Integer, Integer>>> columnTable = new HashMap();
+    int minColumn = 0, maxColumn = 0;
+
+    private void DFS(TreeNode node, Integer row, Integer column) {
+        if (node == null)
+            return;
+
+        if (!columnTable.containsKey(column)) {
+            this.columnTable.put(column, new ArrayList<Pair<Integer, Integer>>());
+        }
+
+        this.columnTable.get(column).add(new Pair<Integer, Integer>(row, node.val));
+        this.minColumn = Math.min(minColumn, column);
+        this.maxColumn = Math.max(maxColumn, column);
+        // preorder DFS traversal
+        this.DFS(node.left, row + 1, column - 1);
+        this.DFS(node.right, row + 1, column + 1);
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> output = new ArrayList();
+        if (root == null) {
+            return output;
+        }
+
+        // step 1). DFS traversal
+        this.DFS(root, 0, 0);
+
+        // step 2). retrieve the value from the columnTable
+        for (int i = minColumn; i < maxColumn + 1; ++i) {
+            // order by both "row" and "value"
+            Collections.sort(columnTable.get(i), new Comparator<Pair<Integer, Integer>>() {
+                @Override
+                public int compare(Pair<Integer, Integer> p1, Pair<Integer, Integer> p2) {
+                    if (p1.getKey().equals(p2.getKey()))
+                        return p1.getValue() - p2.getValue();
+                    else
+                        return p1.getKey() - p2.getKey();
+                }
+            });
+
+            List<Integer> sortedColumn = new ArrayList();
+            for (Pair<Integer, Integer> p : columnTable.get(i)) {
+                sortedColumn.add(p.getValue());
+            }
+            output.add(sortedColumn);
+        }
+
+        return output;
+    }
+}
+
+
+
+
+// Approach 1: BFS/DFS with Global Sorting
+// BFS
+class Triplet<F, S, T> {
+    public final F first;
+    public final S second;
+    public final T third;
+
+    public Triplet(F first, S second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+class Solution {
+    List<Triplet<Integer, Integer, Integer>> nodeList = new ArrayList<>();
+
+    private void BFS(TreeNode root) {
+        Queue<Triplet<TreeNode, Integer, Integer>> queue = new ArrayDeque();
+        int row = 0, column = 0;
+        queue.offer(new Triplet(root, row, column));
+
+        while (!queue.isEmpty()) {
+            Triplet<TreeNode, Integer, Integer> triplet = queue.poll();
+            root = triplet.first;
+            row = triplet.second;
+            column = triplet.third;
+
+            if (root != null) {
+                this.nodeList.add(new Triplet(column, row, root.val));
+                queue.offer(new Triplet(root.left, row + 1, column - 1));
+                queue.offer(new Triplet(root.right, row + 1, column + 1));
+            }
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+        List<List<Integer>> output = new ArrayList();
+        if (root == null) {
+            return output;
+        }
+
+        // step 1). BFS traversal
+        BFS(root);
+
+        // step 2). sort the global list by <column, row, value>
+        Collections.sort(this.nodeList, new Comparator<Triplet<Integer, Integer, Integer>>() {
+            @Override
+            public int compare(Triplet<Integer, Integer, Integer> t1,
+                    Triplet<Integer, Integer, Integer> t2) {
+                if (t1.first.equals(t2.first))
+                    if (t1.second.equals(t2.second))
+                        return t1.third - t2.third;
+                    else
+                        return t1.second - t2.second;
+                else
+                    return t1.first - t2.first;
+            }
+        });
+
+        // step 3). extract the values, partitioned by the column index.
+        List<Integer> currColumn = new ArrayList();
+        Integer currColumnIndex = this.nodeList.get(0).first;
+
+        for (Triplet<Integer, Integer, Integer> triplet : this.nodeList) {
+            Integer column = triplet.first, value = triplet.third;
+            if (column == currColumnIndex) {
+                currColumn.add(value);
+            } else {
+                output.add(currColumn);
+                currColumnIndex = column;
+                currColumn = new ArrayList();
+                currColumn.add(value);
+            }
+        }
+        output.add(currColumn);
+
+        return output;
+    }
+}
+
+
+
+
+// Approach 1: BFS/DFS with Global Sorting
+// DFS
+class Triplet<F, S, T> {
+    public final F first;
+    public final S second;
+    public final T third;
+
+    public Triplet(F first, S second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+class Solution {
+    List<Triplet<Integer, Integer, Integer>> nodeList = new ArrayList<>();
+
+    private void DFS(TreeNode node, Integer row, Integer column) {
+        if (node == null)
+            return;
+        nodeList.add(new Triplet(column, row, node.val));
+        // preorder DFS traversal
+        this.DFS(node.left, row + 1, column - 1);
+        this.DFS(node.right, row + 1, column + 1);
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> output = new ArrayList();
+        if (root == null) {
+            return output;
+        }
+
+        // step 1). DFS traversal
+        DFS(root, 0, 0);
+
+        // step 2). sort the list by <column, row, value>
+        Collections.sort(this.nodeList, new Comparator<Triplet<Integer, Integer, Integer>>() {
+            @Override
+            public int compare(Triplet<Integer, Integer, Integer> t1,
+                    Triplet<Integer, Integer, Integer> t2) {
+                if (t1.first.equals(t2.first))
+                    if (t1.second.equals(t2.second))
+                        return t1.third - t2.third;
+                    else
+                        return t1.second - t2.second;
+                else
+                    return t1.first - t2.first;
+            }
+        });
+
+        // step 3). extract the values, grouped by the column index.
+        List<Integer> currColumn = new ArrayList();
+        Integer currColumnIndex = this.nodeList.get(0).first;
+
+        for (Triplet<Integer, Integer, Integer> triplet : this.nodeList) {
+            Integer column = triplet.first, value = triplet.third;
+            if (column == currColumnIndex) {
+                currColumn.add(value);
+            } else {
+                output.add(currColumn);
+                currColumnIndex = column;
+                currColumn = new ArrayList();
+                currColumn.add(value);
+            }
+        }
+        output.add(currColumn);
+
+        return output;
+    }
+}
